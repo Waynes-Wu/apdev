8d8a2091c612725ae13acd2fa7e95079a5d23797
Wayne-Tam <137274804+Wayne-Tam@users.noreply.github.com>
2023-07-24 17:38:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose")
-
+const qrcode = require("qrcode");
+const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 //test response
-
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended:true
 }))
@@ -17,7 +18,7 @@ app.use(bodyParser.json())
 app.use(express.static('html', { index: 'login.html' }));
 
 // url to the database
-mongoose.connect('mongodb://localhost:27017/E-Tabi_DB')
+mongoose.connect('mongodb://127.0.0.1:27017/E-Tabi_DB')
 const db = mongoose.connection
 
 db.on('error', () => console.log ("Failed to Connect to Database"))
@@ -34,8 +35,11 @@ app.post("/accountcreation", (req, res) => {
     "username": username,
     "password": password,
     "email": email
+
   }
 
+
+  
   db.collection('users').insertOne(personal_data, (error, collection) =>{
     if (error) {
       console.error(error);
@@ -48,13 +52,16 @@ app.post("/accountcreation", (req, res) => {
 
   //after recording data, fresh page
   return res.redirect('login.html')
-
+  
 });
 
 //checks login
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
+
+  const qrCodeURL= '/generated_qrcode/${username}_qrcode.png';
+  res.render('qrcode', { qrCodeURL });
 
   db.collection('users').findOne({ username: username, password: password }, (error, user) => {
     if (error || !user) {
@@ -98,7 +105,18 @@ app.get('/archive', (req, res) => {
   res.sendFile(path.join(__dirname, 'html' ,'/archive.html'));
 });
 
+app.get('/qr', async(req, res) => {
+  
+  const stJson=JSON.stringify("gyg");
+ //const qrCodeFilePath = path.join(__dirname, "qrcodes", `${username}_qrcode.png`);
+  const qrCodeDataUrl = await qrcode.toDataURL(stJson);
+    res.json({ qrCodeDataUrl });
+});
+
+
+
 // ! lel
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
   }); 
